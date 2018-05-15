7a2ddfd209717d1eb44dd299781fc32f3bf20d78
Alexandre Gorius <ihkr@orange.fr>
2018-05-15 15:51:14 +0200

    Fixed https issue with api

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
 import giphy from 'giphy-api';
-import SearchBar from './search_bar.jsx';
-import Gif from './gif.jsx';
-import GifList from './gif_list.jsx';
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      gifs:[],
-      selectedGifId: null//"oOmPLpzgzq4p3PQV2D",
-    }
+      gifs: [],
+      selectedGifId: null // "oOmPLpzgzq4p3PQV2D",
+    };
   }
 
 
-
   search = (query) => {
-    giphy('QlRy3IXfgL4DtPN1O4KAv6WaF6Ac36n7').search({
+    giphy({ apikey: 'QlRy3IXfgL4DtPN1O4KAv6WaF6Ac36n7', https: true }).search({
       q: query,
       rating: 'g'
     }, (error, result) => {
@@ -35,25 +34,19 @@ class App extends Component {
 
 
   render() {
-
-    const gifs = [
-    { id: "oOmPLpzgzq4p3PQV2D" },
-    { id: "xUOwG6r4nPnVPcEKyI" }
-    ];
-
     return (
       <div>
-      <div className="left-scene">
-      <SearchBar searchFunction={this.search}/>
-      <div className="selected-gif">
-      <Gif id={this.state.selectedGifId}/>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
       </div>
-      </div>
-      <div className="right-scene">
-      <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
-      </div>
-      </div>
-      );
+    );
   }
 }
 
